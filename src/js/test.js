145058e76438d1d39fffc985606096a72a8f4c8a
claudiopro <claudio.procida@gmail.com>
2015-09-05 00:02:24 +0200

    Created skeleton project

---
+++ src/js/test.js
@@ -0,0 +1,9 @@
+var test = {
+	foo : function() {
+		return {
+			bar : function() {
+				return 1;
+			}
+		};
+	}
+};
---
+++ test/unit/test.spec.js
@@ -0,0 +1,19 @@
+describe('the test object', function() {
+	it('is defined', function() {
+		expect(test).toBeDefined();
+	});
+	
+	it('has a method foo()', function() {
+		expect(test.foo).toBeDefined();
+		expect(test.foo).toEqual(jasmine.any(Function));
+	});
+	
+	it('the return value of foo() has a method bar()', function() {
+		expect(test.foo()).not.toBeNull();
+		expect(test.foo().bar).toEqual(jasmine.any(Function));
+	});
+	
+	it('the return value of foo().bar() is 1', function() {
+		expect(test.foo().bar()).toEqual(1);
+	});
+});
